24f0672bcf2f36ad547392524f3714c4109e464c
Andrei Kryshtapovich <krisht96@gmail.com>
2018-09-29 13:15:44 +0300

    initial commit
    initialize project.

---
+++ src/main/java/com/bsu/textchat/TextchatApplication.java
@@ -0,0 +1,12 @@
+package com.bsu.textchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextchatApplication.class, args);
+    }
+}
